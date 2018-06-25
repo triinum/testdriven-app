ea10b86dfdbc016bd3284cb343c422045d4a2c63
aegaas <me@aaronegaas.com>
2018-06-25 20:53:15 +0000

    add code coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,6 +1,7 @@
 """ services/users/manage.py """
 
 import unittest
+import coverage
 
 from flask.cli import FlaskGroup
 from project import create_app, db
@@ -8,6 +9,16 @@ from project.api.models import User
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py'
+    ]
+)
+COV.start()
 
 @cli.command()
 def recreate_db():
@@ -29,6 +40,21 @@ def test():
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    return 1
+
+@cli.command()
+def cov():
+    """test the users service with code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     return 1
 
