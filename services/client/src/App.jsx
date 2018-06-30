afb4cf10159279f468dd4ad91e2fd6091d4f1918
aegaas <me@aaronegaas.com>
2018-06-30 06:13:26 +0000

    add NavBar, tests

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import axios from "axios";
 
+import NavBar from "./components/NavBar";
 import UserList from "./components/UserList";
 import AddUser from "./components/AddUser";
 import About from "./components/About";
@@ -12,7 +13,8 @@ class App extends Component {
     this.state = {
       users: [],
       username: "",
-      email: ""
+      email: "",
+      title: 'TestDriven.io'
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -55,28 +57,31 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <br />
-            <Switch>
-              <Route exact path='/' render={() => (
-                <div>
-                  <h1>All Users</h1>
-                  <hr />
-                  <br />
-                  <AddUser
-                    username={this.state.username}
-                    email={this.state.email}
-                    handleChange={this.handleChange}
-                    addUser={this.addUser}
-                  />
-                  <br />
-                  <UserList users={this.state.users} />
-                </div>
-              )} />
-              <Route exact path='/about' render={About} />
-            </Switch>
+      <div>
+        <NavBar title={this.state.title} />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <br />
+              <Switch>
+                <Route exact path='/' render={() => (
+                  <div>
+                    <h1>All Users</h1>
+                    <hr />
+                    <br />
+                    <AddUser
+                      username={this.state.username}
+                      email={this.state.email}
+                      handleChange={this.handleChange}
+                      addUser={this.addUser}
+                    />
+                    <br />
+                    <UserList users={this.state.users} />
+                  </div>
+                )} />
+                <Route exact path='/about' render={About} />
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
--- services/client/src/components/UserList.jsx
+++ services/client/src/components/UserList.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const UserList = (props) => {
-    return (
-        <div>
-            {
-                props.users.map((user) => {
-                    return (
-                        <h4
-                            key={user.id}
-                            className="card card-body bg-light"
-                        >{ user.username }
-                        </h4>
-                    )
-                })
-            }
-        </div>
-    )
+  return (
+    <div>
+      {
+        props.users.map((user) => {
+          return (
+            <h4
+              key={user.id}
+              className="well"
+            >{ user.username }
+            </h4>
+          )
+        })
+      }
+    </div>
+  )
 };
 
 export default UserList;
